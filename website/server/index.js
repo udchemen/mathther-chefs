2a0d9c61e4d27ff3644394b266d309a018d81205
Lucas Campanari <campanari.ls@gmail.com>
2019-11-02 21:47:19 -0400

    clean up

--- website/server/index.js
+++ website/server/index.js
@@ -10,35 +10,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(pino)
 app.use(fileUpload())
 
-app.get('/api/greeting', greeting)
-app.get('/api/hello', callHello)
 app.post('/api/analize', analize)
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 )
 
-function callHello (req, res) {
+function callImageAnalizer (req, res, options) {
   var spawn = require('child_process').spawn
   var process = spawn('python', [
     '-u',
     path.join(__dirname, '../../python/hello.py'),
-    req.query.firstname
+    options.filePath
   ])
 
   process.stdout.on('data', function (data) {
     res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify({ data: data.toString('utf-8') }))
+    res.send(data.toString('utf-8'))
   })
-}
-
-function greeting (req, res) {
-  const name = req.query.name || 'World'
-  res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }))
 }
 
 function analize (req, res) {
   // move uploaded file to folder
-  req.files.file.mv(`${__dirname}/uploads/${req.files.file.name}`)
+  let filePath = `${__dirname}/uploads/${req.files.file.name}`
+
+  req.files.file.mv(filePath, err => {
+    if (err) return res.status(500).send(err)
+
+    // return callImageAnalizer(req, res)
+    return callImageAnalizer(req, res, { filePath })
+  })
 }
