30bad6f9f4d875e9898e546415f241adfd5f32dc
Uyirendiren Chemen <udchemen@gmail.com>
2019-11-02 20:59:13 -0400

    Adding Pixel to length converter

---
+++ website/src/views/UserForm/NewFile 2.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react'
+import { Button, Card, Col, Form, Row, Spinner } from 'react-bootstrap'
+import './newFile.css'
+
+const NewFile = ({ updateList }) => {
+  const [file, setFile] = useState(null)
+  const [uploading, setUploading] = useState(false)
+  //   const { append } = useContext(FilesContext)
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    console.log(e)
+    setUploading(true)
+    const data = new FormData(e.target)
+    // return fetch(`${URL_API}/file`, {
+    //   method: 'post',
+    //   headers: {
+    //     Authorization: `Bearer ${UserControl.getToken()}`
+    //   },
+    //   body: data
+    // })
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     append(res)
+    //     setFile(null)
+    //     setUploading(false)
+    //     document.getElementById('file').value = ''
+    //   })
+    //   .catch(handleSubmitError)
+  }
+
+  function handleSubmitError (err) {
+    console.log(err)
+
+    setFile(null)
+    setUploading(false)
+    document.getElementById('file').value = ''
+    alert('Unexpected error. Please try another file.')
+  }
+
+  function handleChange (e) {
+    const form = document.getElementById('newFile')
+    const data = new FormData(form)
+    data.append('test', 'Hello')
+    setFile(data.get('file').name)
+
+    return fetch(`/api/analize`, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        // 'Content-Type': 'application/json'
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' // otherwise $_POST is empty
+      },
+      body: data
+    })
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     // append(res)
+    //     // setFile(null)
+    //     // setUploading(false)
+    //     // document.getElementById('file').value = ''
+    //   })
+    //   .catch(handleSubmitError)
+  }
+
+  let fileInput
+
+  return (
+    <>
+      <Row className='mt-4'>
+        <Col>
+          <h1 className='font-weight-light mb-4'>New file</h1>
+          <Form id='newFile' onSubmit={handleSubmit}>
+            <Card
+              className={`new-file bg-transparent text-white ${
+                uploading ? 'blocked' : ''
+              }`}
+              onClick={() => (!uploading ? fileInput.click() : null)}
+            >
+              <Card.Body className='d-flex justify-content-center align-items-center'>
+                {file ? (
+                  <>
+                    {file}
+                    {/* <Icon
+                      path={mdiCheck}
+                      size={1}
+                      color='green'
+                      className='ml-2'
+                    /> */}
+                  </>
+                ) : (
+                  'Click here to upload a new file'
+                )}
+              </Card.Body>
+              <Form.Group controlId='file' className='upload-form d-none'>
+                <Form.Control
+                  type='file'
+                  name='file'
+                  ref={ref => (fileInput = ref)}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Card>
+            <Button
+              variant='success'
+              className='mt-3 float-right'
+              type='submit'
+              disabled={uploading || !file}
+              style={{ width: '100px' }}
+            >
+              {uploading ? (
+                <Spinner animation='border' size='sm' as='span' />
+              ) : (
+                'Upload'
+              )}
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default NewFile
---
+++ website/src/views/UserForm/UserForm 2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import NewFile from './NewFile'
+import './userForm.css'
+
+const Form = ({ onclick }) => {
+  function chooseFile () {}
+
+  return (
+    <Container>
+      <NewFile />
+    </Container>
+  )
+}
+
+export default Form
