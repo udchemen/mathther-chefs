fe5dd5882cce32d336e9f605bf0ab6ad08c5fc5b
Uyirendiren Chemen <udchemen@gmail.com>
2019-11-03 09:19:52 -0500

    Suppress prints

--- python/assets/lengthFinder/python/find_px_length.bash
+++ python/assets/lengthFinder/python/find_px_length.bash
@@ -2,6 +2,7 @@
 
 x=$(python3 apriltag.py $1)
 
+
 # echo $x
 
 y=$(python3 corners_dimensions.py $1)
@@ -25,26 +26,26 @@ tr -d '[]' < coords.txt > coords2.txt
 coord1=$(sed -n '1p' <coords2.txt)
 coord1X=$(cut -d',' -f1 <<<"$coord1")
 coord1Y=$(cut -d',' -f2 <<<"$coord1")
-echo $coord1X
-echo $coord1Y
+# echo $coord1X
+# echo $coord1Y
 
 coord2=$(sed -n '2p' <coords2.txt)
 coord2X=$(cut -d',' -f1 <<<"$coord2")
 coord2Y=$(cut -d',' -f2 <<<"$coord2")
-echo $coord2X
-echo $coord2Y
+# echo $coord2X
+# echo $coord2Y
 
 coord3=$(sed -n '3p' <coords2.txt)
 coord3X=$(cut -d',' -f1 <<<"$coord3")
 coord3Y=$(cut -d',' -f2 <<<"$coord3")
-echo $coord3X
-echo $coord3Y
+# echo $coord3X
+# echo $coord3Y
 
 coord4=$(sed -n '4p' <coords2.txt)
 coord4X=$(cut -d',' -f1 <<<"$coord4")
 coord4Y=$(cut -d',' -f2 <<<"$coord4")
-echo $coord4X
-echo $coord4Y
+# echo $coord4X
+# echo $coord4Y
 
 python testLine.py $coord1X $coord1Y $coord2X $coord2Y $coord3X $coord3Y $coord4X $coord4Y $1
 
